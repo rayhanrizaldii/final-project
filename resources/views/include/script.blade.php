b202d63c99d263892ab03826fd230a9350c8b83c
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-28 23:20:59 +0700

    update all

---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,10 @@
+{{-- script --}}
+
+<script src="{{ asset('template/assets/static/js/components/dark.js') }}"></script>
+<script src="{{ asset('template/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+<script src="{{ asset('template/assets/compiled/js/app.js') }}"></script>
+
+<!-- Need: Apexcharts -->
+<script src="{{ asset('template/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('template/assets/static/js/pages/dashboard.js') }}"></script>
