d7067a3d585820d61f12edd8f59369bb730d49c4
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-30 20:31:35 +0700

    Update migration for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('uuid')->primary()->unique();
+            $table->string('nama');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('status');
+            $table->string('nomor_rekening');
+            $table->bigInteger('unit_id')->unique();
             $table->timestamps();
         });
     }
