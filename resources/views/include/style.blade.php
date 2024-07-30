084de57009b4b63879769acdae7a2ce23f0535f9
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-30 21:53:53 +0700

    Update style.blade.php

--- resources/views/include/style.blade.php
+++ resources/views/include/style.blade.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/app.css') }}" />
 <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/app-dark.css') }}" />
 <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/iconly.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/assets/extensions/choices.js/public/assets/styles/choices.css') }}">
 
 {{-- css login --}}
 <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/auth.css') }}">
