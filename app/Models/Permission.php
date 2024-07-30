ee881d7ffeb92c28e4b72f3ec4370e4d5413d20f
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-30 20:32:29 +0700

    Create Permission Model

---
+++ app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+use Illuminate\Support\Str;
+
+class Permission extends SpatiePermission
+{
+    use HasFactory;
+    use HasUuids;
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $primaryKey = 'uuid';
+}
