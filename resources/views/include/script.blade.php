5c673fc1d98995841ce4a19ef569dd697f7d8bc1
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-30 20:30:06 +0700

    Update script.blade.php
    add script for sidebar and alert

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -8,3 +8,44 @@
 <!-- Need: Apexcharts -->
 <script src="{{ asset('template/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
 <script src="{{ asset('template/assets/static/js/pages/dashboard.js') }}"></script>
+
+
+{{-- script sidebar --}}
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const items = document.querySelectorAll('.sidebar-item');
+
+        // Menandai item sidebar yang sesuai dengan URL halaman saat ini sebagai aktif
+        const currentURL = window.location.href;
+        items.forEach(item => {
+            const link = item.querySelector('a.sidebar-link');
+            if (link && link.href === currentURL) {
+                item.classList.add('active');
+            }
+
+            item.addEventListener('click', function() {
+                // Hapus kelas 'active' dari semua item
+                items.forEach(i => i.classList.remove('active'));
+
+                // Tambahkan kelas 'active' pada item yang diklik
+                this.classList.add('active');
+            });
+        });
+    });
+</script>
+
+{{-- script alert --}}
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const statusMessage = document.getElementById('status-message');
+        if (statusMessage) {
+            setTimeout(function() {
+                statusMessage.style.transition = 'opacity 1s'; // Animasi transisi untuk efek yang halus
+                statusMessage.style.opacity = 0;
+                setTimeout(function() {
+                    statusMessage.remove(); // Hapus elemen dari DOM setelah animasi
+                }, 1000); // Waktu yang sama dengan durasi animasi transisi
+            }, 3000); // 3000 ms = 3 detik
+        }
+    });
+</script>
