17139cf6d6f2feb6b7e1aed3f1ff51966deba91f
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-07-30 20:32:40 +0700

    Create Role Model

---
+++ app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as SpatieRole;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Role extends SpatieRole
+{
+    use HasFactory;
+    use HasUuids;
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $primaryKey = 'uuid';
+}
