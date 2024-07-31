3188cd32262dacccb93bc7e5c52fc4a0c372de5e
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-08-01 01:10:54 +0700

    Update UserController.php
    create update and delete function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,6 +55,51 @@ class UserController extends Controller
     public function edit(User $user)
     {
         $roles = Role::pluck('name', 'name')->all();
-        return view('role-permission.user.edit', ['user' => $user, 'roles' => $roles]);
+        $userRoles = $user->roles->pluck('name', 'name')->all();
+        return view(
+            'role-permission.user.edit',
+            [
+                'user' => $user,
+                'roles' => $roles,
+                'userRoles' => $userRoles
+            ]
+        );
+    }
+
+    public function update(Request $request, User $user)
+    {
+
+
+        $request->validate([
+            'nama' => ['required', 'string', 'max:255'],
+            'password' => ['nullable', 'string', 'min:8', 'max:20', 'confirmed'],
+            'status' => ['required', 'boolean'],
+            'nomor_rekening' => ['required', 'string'],
+            'roles' => ['required'],
+            'unit_id' => ['required', 'integer']
+        ]);
+
+        $data = [
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'status' => $request->status,
+            'nomor_rekening' => $request->nomor_rekening,
+            'unit_id' => $request->unit_id,
+        ];
+
+        if (!empty($request->password)) {
+            $data['password'] = Hash::make($request->password);
+        }
+
+        $user->update($data);
+        $user->syncRoles($request->roles);
+
+        return redirect('/users')->with('status', 'User updated successfully with roles');
+    }
+
+    public function destroy($uuid)
+    {
+        User::findOrFail($uuid)->delete();
+        return redirect('/users')->with('status', 'User deleted successfully');
     }
 }
