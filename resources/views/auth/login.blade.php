16d6d5afae93c18edf63d3797fa2ca805d43846a
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-08-01 01:13:56 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,29 +11,36 @@
 <body>
     <script src="assets/static/js/initTheme.js"></script>
     <div id="auth">
-
         <div class="row h-100">
             <div class="col-lg-12 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="index.html"><img src="{{ asset('template/assets/compiled/svg/logo.svg') }}"
+                        <a href="{{ url('login') }}"><img src="{{ asset('template/assets/compiled/svg/logo.svg') }}"
                                 alt="Logo"></a>
                     </div>
                     <h1 class="auth-title">Log in.</h1>
                     <p class="auth-subtitle mb-3">Log in with your data that you entered during registration.</p>
-
-                    <form action="index.html">
+                    <form action="{{ url('login_process') }}" method="POST">
+                        @csrf
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <input type="text" class="form-control form-control-xl" placeholder="Username or Email"
+                                name="email">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password"
+                                name="password">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-check form-check-lg d-flex align-items-end">
                             <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
@@ -41,12 +48,33 @@
                                 Keep me logged in
                             </label>
                         </div>
-                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                        @if (session('error'))
+                            <script>
+                                document.addEventListener('DOMContentLoaded', function() {
+                                    Toast.fire({
+                                        icon: 'error',
+                                        title: '{{ session('error') }}'
+                                    });
+                                });
+                            </script>
+                        @endif
+                        @if (session('success'))
+                            <script>
+                                document.addEventListener('DOMContentLoaded', function() {
+                                    Toast.fire({
+                                        icon: 'success',
+                                        title: '{{ session('success') }}'
+                                    });
+                                });
+                            </script>
+                        @endif
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    @include('include.script')
 </body>
 
 </html>
