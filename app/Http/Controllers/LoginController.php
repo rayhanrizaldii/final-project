b6a4d0764207ace0f5ca7122b2f3562d618d7dc0
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-08-01 01:10:22 +0700

    Update LoginController.php
    create login process and logout function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('auth.login');
+    }
+
+    public function login_process(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required|min:8',
+        ]);
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($data)) {
+            return redirect()->route('dashboard');
+        } else {
+            return redirect()->route('login')->with('error', 'Invalid Email or Password');
+        };
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login')->with('success', 'Logout Successfully');
     }
 }
